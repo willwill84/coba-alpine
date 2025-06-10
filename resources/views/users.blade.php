82c9be57c7679a4b43a4cca1d12d901fc98655fe
William Bania <willwill84@gmail.com>
2025-06-10 08:53:53 +0700

    add form modal

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -10,20 +10,26 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <div x-data='userManagement(@json($users))'>
-                        <!-- Form -->
-                        <div class="mb-6 p-4 border rounded bg-gray-50">
-                            <h2 x-text="isEditing ? 'Edit User' : 'Add User'" class="text-lg font-semibold mb-2"></h2>
-                            <div class="space-y-2">
-                                <input type="text" x-model="form.name" x-ref="nameInput" placeholder="Name" class="w-full mb-2 p-2 border rounded">
-                                <div x-show="errors.name" class="text-red-500 text-sm" x-text="errors.name ? errors.name[0] : ''"></div>
 
-                                <input type="email" x-model="form.email" placeholder="Email" class="w-full mb-2 p-2 border rounded">
-                                <div x-show="errors.email" class="text-red-500 text-sm" x-text="errors.email ? errors.email[0] : ''"></div>
+                        <div class="flex justify-end mb-4">
+                            <button x-on:click="$refs.userDialogRef.showModal()" class="px-3 py-2 bg-blue-500 text-white rounded">Add User</button>
+                            <dialog @click="closeFromEvent(event)" x-ref="userDialogRef" class="fixed bg-white rounded-lg shadow-lg max-w-md w-full p-4">
+                                <!-- Form -->
+                                <div class="mb-6 p-4 border rounded bg-gray-50">
+                                    <h2 x-text="isEditing ? 'Edit User' : 'Add User'" class="text-lg font-semibold mb-2"></h2>
+                                    <div class="space-y-2">
+                                        <input type="text" x-model="form.name" x-ref="nameInput" placeholder="Name" class="w-full mb-2 p-2 border rounded">
+                                        <div x-show="errors.name" class="text-red-500 text-sm" x-text="errors.name ? errors.name[0] : ''"></div>
 
-                                <button @click="saveUser" x-text="isEditing ? 'Update' : 'Add User'" :disabled="isLoading" x-bind:class="isEditing ? 'bg-blue-500 hover:bg-blue-600' : 'bg-green-500 hover:bg-green-600'" class="px-3 py-1  text-white rounded">Update</button>
-                                <button @click="cancelEdit" class="ml-2 px-3 py-1 bg-gray-400 text-white rounded hover:bg-gray-500">Cancel</button>
-                                <span x-show="isLoading" class="ml-2 text-sm text-gray-600 italic">Loading...</span>
-                            </div>
+                                        <input type="email" x-model="form.email" placeholder="Email" class="w-full mb-2 p-2 border rounded">
+                                        <div x-show="errors.email" class="text-red-500 text-sm" x-text="errors.email ? errors.email[0] : ''"></div>
+
+                                        <button @click="saveUser" x-text="isEditing ? 'Update' : 'Add User'" :disabled="isLoading" x-bind:class="isEditing ? 'bg-blue-500 hover:bg-blue-600' : 'bg-green-500 hover:bg-green-600'" class="px-3 py-1  text-white rounded">Update</button>
+                                        <button @click="cancelEdit" class="ml-2 px-3 py-1 bg-gray-400 text-white rounded hover:bg-gray-500">Cancel</button>
+                                        <span x-show="isLoading" class="ml-2 text-sm text-gray-600 italic">Loading...</span>
+                                    </div>
+                                </div>
+                            </dialog>
                         </div>
 
                         <!-- Daftar Users -->
@@ -101,7 +107,6 @@
                 this.isLoading = true;
                 axios.get(`${this.pagination.path}?page=${page}`)
                     .then(response => {
-                        console.log(response.data);
                         this.users = response.data.data;
                         this.pagination.current_page = response.data.current_page;
                         this.pagination.last_page = response.data.last_page;
@@ -132,11 +137,13 @@
                 name: '',
                 email: ''
             },
+            errors: {}, // <-- Untuk simpan error dari backend
+
             resetForm() {
+                this.isEditing = false;
                 this.form = { id: null, name: '', email: '' };
                 this.errors = {};
             },
-            errors: {}, // <-- Untuk simpan error dari backend
 
             saveUser() {
                 if (this.isEditing) {
@@ -163,8 +170,10 @@
                         // this.users.unshift(response.data.user);
                         this.loadPage(this.pagination.current_page);
                         this.pagination.total += 1;
-                        this.resetForm();
-                        this.$refs.nameInput.focus();
+                        // this.resetForm();
+                        // this.$refs.nameInput.focus();
+                        this.close();
+
                     }
                 } catch (error) {
                     if (error.response && error.response.status === 422) {
@@ -181,6 +190,8 @@
                 this.isEditing = true;
                 this.errors = {};
                 this.form = { ...user };
+
+                this.$refs.userDialogRef.showModal();
                 this.$refs.nameInput.focus();
             },
 
@@ -194,6 +205,7 @@
                 const user = this.users.find(u => u.id === this.form.id);
                 if (user && this.form.name === user.name && this.form.email === user.email) {
                     alert('Tidak ada perubahan data.');
+                    this.close();
                     return;
                 }
 
@@ -207,8 +219,8 @@
                             this.users[index] = response.data.user;
                         }
 
-                        this.resetForm();
-                        this.isEditing = false;
+                        this.close();
+
                         alert(response.data.message);
                     }
                 } catch (error) {
@@ -223,8 +235,18 @@
             },
 
             cancelEdit() {
-                this.isEditing = false;
+                this.close();
+            },
+
+            closeFromEvent(event) {
+                if (event.currentTarget === event.target) {
+                    this.close();
+                }
+            },
+
+            close() {
                 this.resetForm();
+                this.$refs.userDialogRef.close();
             },
 
             async deleteUser(userId) {
